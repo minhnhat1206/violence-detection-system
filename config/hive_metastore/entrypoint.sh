b991aa18166ddef2e7ebacbc0a6b0f689a3a4c2c
minhnhat1206 <minhnhatnhc@gmail.com>
2025-11-23 19:01:16 +0700

    extract minio image

--- config/hive_metastore/entrypoint.sh
+++ config/hive_metastore/entrypoint.sh
@@ -9,6 +9,10 @@ WAIT_FOR_HOST=${WAIT_FOR_HOST:-mysql}
 WAIT_FOR_PORT=${WAIT_FOR_PORT:-3306}
 WAIT_TIMEOUT=${WAIT_TIMEOUT:-120}
 
+MYSQL_USER=${METASTORE_DB_USER:-root}
+MYSQL_PASS=${METASTORE_DB_PASSWORD:-root}
+MYSQL_DB=${METASTORE_DB_NAME:-metastore}
+
 echo "[entrypoint] Hive Metastore startup wrapper"
 echo "[entrypoint] HIVE_CONF_DIR=${HIVE_CONF_DIR}, ACTION=${ACTION}"
 
@@ -36,7 +40,8 @@ run_schematool() {
 
 if [ "$ACTION" = "init" ]; then
   echo "[entrypoint] Checking if Hive schema exists..."
-  if "${HIVE_HOME}/bin/schematool" -dbType mysql -info | grep -q "No schemas found"; then
+  if ! mysql -h ${WAIT_FOR_HOST} -u${MYSQL_USER} -p${MYSQL_PASS} ${MYSQL_DB} -e "SELECT * FROM VERSION;" >/dev/null 2>&1; then
+    echo "[entrypoint] Hive schema not found, initializing..."
     run_schematool initSchema
   else
     echo "[entrypoint] Hive schema already exists, skipping init"
