f73b625467a48bb368da3b3c44d28f70402034a8
minhnhat1206 <minhnhatnhc@gmail.com>
2025-11-27 18:03:41 +0700

    hide grafana's panel

--- frontend/vigilance-ai_-violence-detection-dashboard/components/pages/Analytics.tsx
+++ frontend/vigilance-ai_-violence-detection-dashboard/components/pages/Analytics.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React from "react";
 
-const Analytics: React.FC = () => {
+const Analytics = () => {
   return (
     <div className="bg-slate-900/50 p-6 rounded-xl border border-slate-800 shadow-lg">
       <div className="h-[800px] w-full">
         <iframe
-          src="http://localhost:3001/dashboard/snapshot/im9QlXQVi4JUXziOmQouAHtVNiMctokh"
+          src="http://localhost:3001/dashboard/snapshot/im9QlXQVi4JUXziOmQouAHtVNiMctokh?kiosk"
           width="100%"
           height="100%"
           frameBorder="0"
-        ></iframe>
+          allowFullScreen
+        />
       </div>
     </div>
   );
